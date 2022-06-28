35a862f3c4873a6e7ed2ba4edcce5333dfb7de9d
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-06-28 12:35:53 -0300

    fix: vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 import express, { json } from "express";
 import cors from "cors";
 
-const app = express();
+export const app = express();
 app.use(cors());
 app.use(json());
 
-app.get("/api/login", (req, res) => {
-    res.send("olá");
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
 });
+
+app.get("/api/login", (req, res) => {
+    res.send("login");
+});
+
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+});
+
