8f084e528aca3b075fc082ec1cf83224eb7fc9fe
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-04 20:38:32 -0300

    fix: db name

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -1,9 +1,6 @@
 import { MongoClient } from "mongodb";
 
-
-
-
 const client = new MongoClient(process.env.DB_URI);
 
 client.connect();
-export let db = client.db("mywalletcluster");
+export let db = client.db("MyWalletCluster");
